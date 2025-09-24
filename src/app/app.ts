d742319645cdc34aa4074948fc5e7f0a87510cb3
StefanoR <rubinetti.stefano@gmail.com>
2025-09-24 14:56:42 +0200

    a

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('EsempioSemiCompleto');
+export class App
+{
+  constructor(private http:HttpClient) {
+  }
 }
